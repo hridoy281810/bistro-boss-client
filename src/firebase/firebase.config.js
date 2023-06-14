4a5853fccfa4dfceafb4aeee05ddd1bbd0945152
hridoy281810 <hridoy01724281810@gmail.com>
2023-06-14 18:54:27 +0600

    add send mail system

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -19,24 +19,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
-
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBsAPegNJWJCmbK7Kij4Q5JzRZnzlDXrMY",
-//   authDomain: "bistro-boss-e161f.firebaseapp.com",
-//   projectId: "bistro-boss-e161f",
-//   storageBucket: "bistro-boss-e161f.appspot.com",
-//   messagingSenderId: "888696186796",
-//   appId: "1:888696186796:web:59dcafd322f5643f2f3c09"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
